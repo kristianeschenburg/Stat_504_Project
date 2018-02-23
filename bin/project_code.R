aec52aae0e670cd3380b69bb9b6921b943069b32
unknown <epic19mz@hotmail.com>
2018-02-23 11:41:34 +0800

    change data to numeric and imputatation data by hot.deck

--- bin/project_code.R
+++ bin/project_code.R
@@ -1,4 +1,28 @@
-test <- function(a,b) {
-  
-  return a + b
-}
+data1=read.table(file.choose(),header=T,sep=",")
+data2<-as.matrix(data1)
+colnames(data2)[1]<-"age"
+data3<-data.frame(data2)
+h1<-hot.deck(data3,method='p.draw')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
